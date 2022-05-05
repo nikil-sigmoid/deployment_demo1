930994ead46dfd57c3ca1bacae0eb779599fe32b
nikil-sigmoid <88651182+nikil-sigmoid@users.noreply.github.com>
2022-05-05 13:46:26 +0530

    Update changed_files.sh

--- changed_files.sh
+++ changed_files.sh
@@ -17,22 +17,23 @@ echo "Final changed files detected"
 echo $RESULT
 mkdir -p $TEMP_DIR_PATH
 my_array=($(echo $RESULT | tr ";" "\n"))
-for file_name in "${my_array[@]}";do
-   echo "::set-output name=VALIDATE_CHANGES_SCHEMACHANGE::TRUE"
-   EPOCH_TIMESTAMP_WITH_MILLIS=".$(date +%s.%3N)_"
-   echo "Original file name with path $file_name"
-   EXTRACTED_FILE_NAME=$(echo "$file_name" | sed "s/.*\///")
-   echo "Extracted file name only $EXTRACTED_FILE_NAME"
-   if [[ $EXTRACTED_FILE_NAME == V* ]]; then
-        echo "Matched"
-        REPLACED_FILE_NAME=${EXTRACTED_FILE_NAME/_/$EPOCH_TIMESTAMP_WITH_MILLIS}
-        EXTRACTED_FILE_NAME=$REPLACED_FILE_NAME
-        echo "Replaced file name $EXTRACTED_FILE_NAME"
-   fi
-   cp "$file_name" "$TEMP_DIR_PATH/$EXTRACTED_FILE_NAME"
-   sleep 0.0001
-   echo "============================================="
-done
+echo "::set-output name=VALIDATE_CHANGES_SCHEMACHANGE::${my_array[@]}"
+# for file_name in "${my_array[@]}";do
+#    echo "::set-output name=VALIDATE_CHANGES_SCHEMACHANGE::TRUE"
+#    EPOCH_TIMESTAMP_WITH_MILLIS=".$(date +%s.%3N)_"
+#    echo "Original file name with path $file_name"
+#    EXTRACTED_FILE_NAME=$(echo "$file_name" | sed "s/.*\///")
+#    echo "Extracted file name only $EXTRACTED_FILE_NAME"
+#    if [[ $EXTRACTED_FILE_NAME == V* ]]; then
+#         echo "Matched"
+#         REPLACED_FILE_NAME=${EXTRACTED_FILE_NAME/_/$EPOCH_TIMESTAMP_WITH_MILLIS}
+#         EXTRACTED_FILE_NAME=$REPLACED_FILE_NAME
+#         echo "Replaced file name $EXTRACTED_FILE_NAME"
+#    fi
+#    cp "$file_name" "$TEMP_DIR_PATH/$EXTRACTED_FILE_NAME"
+#    sleep 0.0001
+#    echo "============================================="
+# done
 
 echo "Files copied to tmp location $TEMP_DIR_PATH"
 ls -ltr $TEMP_DIR_PATH
